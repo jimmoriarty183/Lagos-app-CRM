0b615b30b1789a634737870a88fd3ef7fd11cef7
jimmoriarty183 <aleksandr1krupych@gmail.com>
2026-03-28 01:52:09 +0200

    Add dynamic export to reset password page

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,5 +1,7 @@
 import ResetPasswordUI from "./ui";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <div className="min-h-[100svh] bg-gradient-to-br from-gray-50 via-blue-50/20 to-gray-50">
